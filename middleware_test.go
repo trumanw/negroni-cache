54f23b70c14ca4c5b09a45f58cd3836a84de234e
truman-misfit <truman@misfit.com>
2016-12-23 00:06:46 +0800

    Fix the goroutine testing bug through time.Sleep(500 milliseconds).

--- middleware_test.go
+++ middleware_test.go
@@ -1,41 +1,46 @@
 package negronicache
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
 	"net/http/httptest"
+	"testing"
+	"time"
 
-    "github.com/urfave/negroni"
 	"github.com/stretchr/testify/assert"
+	"github.com/urfave/negroni"
 )
 
 func setupServeHTTP(t *testing.T) (negroni.ResponseWriter, *http.Request) {
-    req, err := http.NewRequest("GET", "http://example.com/stuff?rly=ya", nil)
-    assert.Nil(t, err)
+	req, err := http.NewRequest("GET", "http://example.com/stuff?rly=ya", nil)
+	assert.Nil(t, err)
 
-    req.RequestURI = "http://example.com/stuff?rly=ya"
-    req.Method = "GET"
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("Accept", "application/json")
-    req.Header.Set("ETag", "15f0fff99ed5aae4edffdd6496d7131f")
+	req.RequestURI = "http://example.com/stuff?rly=ya"
+	req.Method = "GET"
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("ETag", "15f0fff99ed5aae4edffdd6496d7131f")
 
-    return negroni.NewResponseWriter(httptest.NewRecorder()), req
+	return negroni.NewResponseWriter(httptest.NewRecorder()), req
 }
 
 func TestMiddleware_ServeHTTP(t *testing.T) {
-    mw := NewMiddleware(NewMemoryCache())
+	mw := NewMiddleware(NewMemoryCache())
 
-    recNoCache, reqNoCache := setupServeHTTP(t)
-    mw.ServeHTTP(recNoCache, reqNoCache, func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(200)
-    })
-    assert.Equal(t, recNoCache.Status(), 200)
-    assert.Equal(t, recNoCache.Header().Get(CacheHeader), "SKIP")
+	recNoCache, reqNoCache := setupServeHTTP(t)
+	mw.ServeHTTP(recNoCache, reqNoCache, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+	assert.Equal(t, recNoCache.Status(), 200)
+	assert.Equal(t, recNoCache.Header().Get(CacheHeader), "SKIP")
 
-    recCache, reqCache := setupServeHTTP(t)
-    mw.ServeHTTP(recCache, reqCache, func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(200)
-    })
-    assert.Equal(t, recCache.Status(), 200)
-    assert.Equal(t, recCache.Header().Get(CacheHeader), "HIT")
+	// Sleep a while for async caching the last request in a goroutines
+	time.Sleep(500 * time.Millisecond)
+
+	recCache, reqCache := setupServeHTTP(t)
+	mw.ServeHTTP(recCache, reqCache, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	assert.Equal(t, recCache.Status(), 200)
+	assert.Equal(t, recCache.Header().Get(CacheHeader), "HIT")
 }
