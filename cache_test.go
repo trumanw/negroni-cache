b5cf04e6709bca6c29d1b42d4ae9a0fa6d02c222
truman-misfit <truman@misfit.com>
2016-12-23 18:29:43 +0800

    Add test functions for allocate a cache instance.

--- cache_test.go
+++ cache_test.go
@@ -1,1 +1,15 @@
 package negronicache
+
+func TestCache_NewMemoryCache(t testing.T) {
+    c := NewMemoryCache()
+    assert.NotNil(t, c.fs)
+    assert.NotNil(t, c.stale)
+}
+
+func TestCache_NewDiskCache(t testing.T) {
+    c, err := NewDiskCache("./cache")
+    assert.Nil(t, err)
+
+    assert.NotNil(t, c.fs)
+    assert.NotNil(t, c.stale)
+}
