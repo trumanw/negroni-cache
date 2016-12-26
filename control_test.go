7dabc1630d0015228b46153ea29fd348f9d3db89
truman-misfit <truman@misfit.com>
2016-12-26 12:42:02 +0800

    Add coverage tests on control.go

--- control_test.go
+++ control_test.go
@@ -1,1 +1,21 @@
 package negronicache
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func MockCacheControl(t *testing.T) CacheControl {
+    cacheControlString := "max-age=0,no-cache,no-store"
+    cacheControl, err := ParseCacheControl(cacheControlString)
+    assert.Nil(t, err)
+
+    return cacheControl
+}
+
+func TestControl_String(t *testing.T) {
+    cacheControl := MockCacheControl(t)
+    cacheControlString := cacheControl.String()
+    assert.NotNil(t, cacheControlString)
+}
